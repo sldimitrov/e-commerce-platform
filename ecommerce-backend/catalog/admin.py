2a7c701c2138309c54086664957384d2746b182d
Slavi FE <slavidimitrov54@gmail.com>
2026-07-16 14:59:38 +0300

    Initialise the base models for the Catalog and admin

--- ecommerce-backend/catalog/admin.py
+++ ecommerce-backend/catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, ProductVariant, Product, Inventory
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ProductVariant)
+class ProductVariantAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    pass
--- ecommerce-backend/catalog/models.py
+++ ecommerce-backend/catalog/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from core.models import TimeStampedModel
 
-# Create your models here.
+
+class Category(TimeStampedModel):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'parent'],
+                name="unique_category_per_parent"
+            )
+        ]
+
+
+class Product(TimeStampedModel):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
+    categories = models.ManyToManyField(Category, related_name='products')
+
+    def __str__(self):
+            return self.name
+
+
+class ProductVariant(TimeStampedModel):
+    product = models.ForeignKey(Product, related_name='variants', on_delete=models.CASCADE)
+    sku = models.CharField(max_length=100, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.product.name
+
+
+class Inventory(TimeStampedModel):
+    variant = models.ForeignKey(ProductVariant, related_name='inventories', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.variant.sku} ({self.quantity})"
