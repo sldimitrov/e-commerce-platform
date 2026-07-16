9a9f52605c089b8b3a9230b9e205b929bcc01ee4
Slavi FE <slavidimitrov54@gmail.com>
2026-07-16 17:26:18 +0300

    add some MUI components to the header

--- ecommerce-frontend/src/components/ui/Header/Header.tsx
+++ ecommerce-frontend/src/components/ui/Header/Header.tsx
@@ -1,20 +1,57 @@
-import {Button} from "@mui/material";
-import {Link} from "react-router";
+import {
+    AppBar,
+    TextField,
+    Toolbar,
+    InputAdornment
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ForestIcon from "@mui/icons-material/Forest";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import Person2Icon from "@mui/icons-material/Person2";
+
+import { useState } from "react";
 
 export default function Header() {
-    return (
-        <>
-            <span>
-                BurgasFrame Shop
-            </span>
+    const [search, setSearch] = useState("");
 
-            <li>
-                <Button>
-                    <Link to={'/'}>
-                        Home
-                    </Link>
-                </Button>
-            </li>
-        </>
-    )
+    const handleSearch = (event) => {
+        setSearch(event.target.value);
+        console.log(event.target.value);
+    };
+
+    return (
+        <AppBar>
+            <Toolbar>
+                <ForestIcon />
+
+                navigation
+
+                <TextField
+                    value={search}
+                    onChange={handleSearch}
+                    placeholder="Search..."
+                    variant="outlined"
+                    size="small"
+                    sx={{
+                        backgroundColor: "white",
+                        borderRadius: 1,
+                        marginLeft: 2,
+                        width: 300
+                    }}
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <SearchIcon />
+                            </InputAdornment>
+                        )
+                    }}
+                />
+
+                <FavoriteIcon />
+                <ShoppingBagIcon />
+                <Person2Icon />
+            </Toolbar>
+        </AppBar>
+    );
 }
